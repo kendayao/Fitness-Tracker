0a88eefabbaf04aab4155893cd6f08341da236b6
Kaeneth Dayao <kendayao@gmail.com>
2020-06-13 21:10:06 -0700

    setting up html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+module.exports=function(app){
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "./public/index.html")
+});
+
+app.get("/exercise", function(req,res){
+    res.sendFile(__dirname + "/public/exercise.html")
+});
+
+
+app.get("/stats", function(req,res){
+    res.sendFile(__dirname + "./public/stats.html")
+});
+
+
+};
--- server.js
+++ server.js
@@ -12,10 +12,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
 app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", {useUnifiedTopology: true, useNewUrlParser: true});
 
 app.listen(PORT, function(){
     console.log(`App running on port ${PORT}!`)
 })
+
