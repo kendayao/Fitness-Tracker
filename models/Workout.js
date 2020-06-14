048eeb97ef923ea7276f11cfb40e04298ab1f123
Kaeneth Dayao <kendayao@gmail.com>
2020-06-14 11:46:49 -0700

    created workout schema

---
+++ models/Workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const Schema = mongoose. Schema;
+
+const WorkoutSchema= new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    excercises: [
+        {
+        type:{
+            type: String,
+            required:[true, "Need a type of excercise"]
+        },
+        name:{
+            type: String,
+            required:[true, "Need name of excerise"]
+        },
+        duration:{
+            type: Number,
+            required:[true,"Need a duration"]
+        },
+        weight:{
+            type: Number,
+        },
+        reps:{
+            type: Number,
+        },
+        sets:{
+            type: Number,
+        },
+        distance:{
+            type: Number,
+        }
+    }
+    ]
+});
+
+const Workout=mongoose.model("Workout", WorkoutSchema);
+
+module.exports=Workout
--- server.js
+++ server.js
@@ -11,16 +11,13 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-
 app.use(express.static("public"));
 
-
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", {useUnifiedTopology: true, useNewUrlParser: true});
+const db=require("./models")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useUnifiedTopology: true, useNewUrlParser: true});
 
 require("./routes/html-routes.js")(app);
-require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 app.listen(PORT, function(){
     console.log(`App running on port ${PORT}!`)
