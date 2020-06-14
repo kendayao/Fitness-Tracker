bc59b7bec27f3b6df5ba990803e0b6f964f6bc84
Kaeneth Dayao <kendayao@gmail.com>
2020-06-13 20:16:27 -0700

    server.js server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+// require npm packages
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", {useUnifiedTopology: true, useNewUrlParser: true});
+
+app.listen(PORT, function(){
+    console.log(`App running on port ${PORT}!`)
+})
